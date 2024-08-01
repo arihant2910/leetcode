1a464bc3143e376926423beac6a23ba5347c8410
Arihant Kulshrestha <90741836+arihant2910@users.noreply.github.com>
2024-08-01 14:48:57 +0530

    Update Solution.java

--- solution/2600-2699/2678.Number of Senior Citizens/Solution.java
+++ solution/2600-2699/2678.Number of Senior Citizens/Solution.java
@@ -1,12 +1,18 @@
 class Solution {
     public int countSeniors(String[] details) {
-        int ans = 0;
-        for (var x : details) {
-            int age = Integer.parseInt(x.substring(11, 13));
-            if (age > 60) {
-                ++ans;
+        int old=0;
+
+        int n =details.length;
+
+        for (String s:details){
+            int tensplace=(s.charAt(11)-'0')*10;
+            int firstplace=s.charAt(12)-'0';
+            int age=tensplace+firstplace;
+            if(age>60){
+                old++;
             }
+            
         }
-        return ans;
+        return old;
     }
 }
